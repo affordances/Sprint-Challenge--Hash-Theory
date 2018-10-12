289f6268f21fe0416d5a3d78d0820ace73194b3c
Gabriel Duquette <gabrielduquette@gmail.com>
2018-10-12 12:08:57 -0400

    hash tables and regex done

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,17 @@
-def get_indices_of_item_weights(weights, limit):
-  pass 
+def reconstruct_trip(tickets):
+    flights = {}
+    itinerary = []
+    for ticket in tickets:
+        flights[ticket[0]] = ticket[1]
+    current_ticket = flights[None]
+    while current_ticket != None:
+        itinerary.append(current_ticket)
+        if current_ticket not in flights:
+            return []
+        current_ticket = flights[current_ticket]
+    return itinerary
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  pass
---
+++ theory/regex_answers.md
@@ -0,0 +1,5 @@
+1. /antelopes?\s?rocks?\s?out/
+
+2. /[g|m]oat/
+
+3. /\d{1,4}-\d{1,2}-\d{1,2}/
