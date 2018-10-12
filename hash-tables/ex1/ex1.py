cfb88eeab0fd9fbb6b84ba9fda33f5e38bad9b56
Gabriel Duquette <gabrielduquette@gmail.com>
2018-10-12 12:15:13 -0400

    whoops

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,15 +1,15 @@
-def reconstruct_trip(tickets):
-    flights = {}
-    itinerary = []
-    for ticket in tickets:
-        flights[ticket[0]] = ticket[1]
-    current_ticket = flights[None]
-    while current_ticket != None:
-        itinerary.append(current_ticket)
-        if current_ticket not in flights:
-            return []
-        current_ticket = flights[current_ticket]
-    return itinerary
+def get_indices_of_item_weights(weights, limit):
+    candidates = {}
+    for i, weight in enumerate(weights):
+        candidates[weight] = i
+    for i, weight in enumerate(weights):
+        if limit - weight in candidates:
+            j = candidates[limit - weight]
+            if i > j:
+                return (i, j)
+            else:
+                return (j, i)
+    return ()
 
 
 if __name__ == '__main__':
